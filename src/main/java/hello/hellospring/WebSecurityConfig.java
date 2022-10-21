c5cac98db0eaab628d1feaeba395e11883d316dd
JYKANG <xadamantiumx@naver.com>
2022-10-21 18:24:43 +0900

    2022-1021

--- src/main/java/hello/hellospring/WebSecurityConfig.java
+++ src/main/java/hello/hellospring/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests() // 6
-                .antMatchers("/login", "/signup", "/").permitAll() // 누구나 접근 허용
+                .antMatchers("/login", "/signup", "/", "/review", "/search", "/ticket", "/ticketmain", "/suggest", "/send1", "/send2").permitAll()
                 .antMatchers("/user").hasRole("USER") // USER, ADMIN만 접근 가능
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
