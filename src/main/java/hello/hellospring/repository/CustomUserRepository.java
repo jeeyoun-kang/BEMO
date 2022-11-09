38d89e94da2a34718377ea5de236d02d77ce961b
JYKANG <xadamantiumx@naver.com>
2022-11-09 20:19:20 +0900

    1109-2019

--- src/main/java/hello/hellospring/repository/CustomUserRepository.java
+++ src/main/java/hello/hellospring/repository/CustomUserRepository.java
@@ -1,9 +1,16 @@
 package hello.hellospring.repository;
 
+import hello.hellospring.entity.Authentication;
+import hello.hellospring.entity.Password;
 import hello.hellospring.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomUserRepository {
     Optional<User> findUserByUsername(String name);
+
+
+
+
 }
