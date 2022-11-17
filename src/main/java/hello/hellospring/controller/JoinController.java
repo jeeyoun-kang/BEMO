95af5d803f49023f8574f7a73ae8ea0d9826d7a4
JYKANG <xadamantiumx@naver.com>
2022-11-17 16:16:24 +0900

    1117-1616

--- src/main/java/hello/hellospring/controller/JoinController.java
+++ src/main/java/hello/hellospring/controller/JoinController.java
@@ -1,21 +1,39 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.dto.AuthenticationDto;
 import hello.hellospring.entity.Authentication;
-import hello.hellospring.service.UserService;
+import hello.hellospring.repository.AuthenticationRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+
+@Slf4j
 @Controller
 public class JoinController {
 
+    @Autowired
+    private AuthenticationRepository authenticationRepository;
+
     @GetMapping("/join")
-    public String login(){
+    public String join() {
         return "join";
     }
+
+    @RequestMapping(value = "/jointwo",method = RequestMethod.POST)
+    public Long jointwo( @RequestBody AuthenticationDto form) { //json 데이터를 받아온다.
+        log.info(form.toString());
+        //1.dto를 엔티티 변환
+        Authentication authentication = form.toEntity();
+        System.out.println("hi");
+        //repository에게 (db-관리-객체) 전달
+        Authentication saved = authenticationRepository.save(authentication);
+        log.info(saved.toString());
+
+        return saved.getAuth_id();
+    }
+
 
 }
 
