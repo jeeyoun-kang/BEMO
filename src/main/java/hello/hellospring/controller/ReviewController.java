4115f6db2a1027d17d663e68f68637cdde7a28bf
JYKANG <xadamantiumx@naver.com>
2022-11-24 16:27:17 +0900

    1124-1627

--- src/main/java/hello/hellospring/controller/ReviewController.java
+++ src/main/java/hello/hellospring/controller/ReviewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ReviewController {
     @GetMapping("review")
     public String review(Model model) {
-        model.addAttribute("data","hello!!");
+
         return "review";
 
 
