b5342c625d096ef0b91f72f969c7e83976ab91c2
JYKANG <xadamantiumx@naver.com>
2022-12-08 17:44:20 +0900

    리뷰페이지구현

--- src/main/java/hello/hellospring/controller/ReviewController.java
+++ src/main/java/hello/hellospring/controller/ReviewController.java
@@ -2,11 +2,13 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReviewController {
-    @GetMapping("review")
-    public String review(Model model) {
+    @GetMapping("/{movie_title}/review")
+    public String review(Model model, @PathVariable String movie_title) {
+        model.addAttribute("movie_title",movie_title);
 
         return "review";
 
