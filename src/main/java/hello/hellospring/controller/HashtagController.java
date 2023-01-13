2626e2e98d4465fedb39febd73935443b36ca234
JYKANG <xadamantiumx@naver.com>
2023-01-13 14:17:12 +0900

    메인페이지 kobisapi ignore

--- src/main/java/hello/hellospring/controller/HashtagController.java
+++ src/main/java/hello/hellospring/controller/HashtagController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HashtagController {
     @GetMapping("/hashtag")
     public String hello(Model model) {
-        model.addAttribute("data","hello!!");
+        
         return "hashtag";
 
 
