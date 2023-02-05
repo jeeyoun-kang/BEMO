ee4e6c3630ad7666c8307298ab872acbdc78bd1c
JYKANG <xadamantiumx@naver.com>
2023-02-06 01:20:45 +0900

    0206-0120

--- src/main/java/bemo/bemo/entity/Posts.java
+++ src/main/java/bemo/bemo/entity/Posts.java
@@ -31,6 +31,8 @@ public class Posts {
     private String update_date;
     private String url;
 
+    private String mvurl;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_no")
     private User user;
@@ -39,7 +41,7 @@ public class Posts {
     private List<Hashtags> hashtags = new ArrayList<>();
 
     @Builder
-    public Posts(String title, String content, String author, String authorname, String mvtitle, String upload_date, String update_date,String url, List<Hashtags> hashtags) {
+    public Posts(String title, String content, String author, String authorname, String mvtitle, String upload_date, String update_date,String url,String mvurl,List<Hashtags> hashtags) {
         this.title = title;
         this.content = content;
         this.author = author;
@@ -48,12 +50,13 @@ public class Posts {
         this.upload_date = upload_date;
         this.update_date = update_date;
         this.url = url;
+        this.mvurl=mvurl;
         for(int i = 0; i<hashtags.size(); i++) {
             addHashtags(hashtags.get(i));
         }
     }
     @Builder(builderMethodName = "builderWithoutHashtag", buildMethodName = "buildWithoutHashtag")
-    public Posts(String title, String content, String author, String authorname, String mvtitle, String upload_date, String update_date,String url) {
+    public Posts(String title, String content, String author, String authorname, String mvtitle, String upload_date, String update_date,String url,String mvurl) {
         this.title = title;
         this.content = content;
         this.author = author;
@@ -62,6 +65,7 @@ public class Posts {
         this.upload_date = upload_date;
         this.update_date = update_date;
         this.url = url;
+        this.mvurl=mvurl;
     }
     public void update(String title, String content, String url,List<Hashtags> hashtags, String update_date) {
         this.title = title;
