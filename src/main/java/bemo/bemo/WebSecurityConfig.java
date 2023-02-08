33555a213d9b8f75dae2be82b930d7dba5b3ba6a
JYKANG <xadamantiumx@naver.com>
2023-02-08 22:05:48 +0900

    0208-2205

--- src/main/java/bemo/bemo/WebSecurityConfig.java
+++ src/main/java/bemo/bemo/WebSecurityConfig.java
@@ -47,11 +47,10 @@ public class WebSecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login","/profile", "/signup", "/", "/review", "/searchfirst","/search", "/hashtag",
-                        "/ticketmain", "/send", "/send2","/join", "/error", "/loginSuccess","/moviedetail",
-                        "/moviedetail/{detail}","/moviedetail/{detail}/{code}","/{movie_title}/review",
-                        "/{movie_title}/review/posts","/comments/{id}","/hello","/api/upload","/api/list",
-                        "/modify/{id}","/users/{id}","/users").permitAll() // 누구나 접근 허용
+                .antMatchers("/login","/profile", "/signup", "/", "/review", "/search",
+                         "/send", "/join", "/error", "/loginSuccess","/moviedetail/{detail}",
+                        "/moviedetail/{detail}/{code}","/{movie_title}/review",
+                        "/comments/{id}","/modify/{id}","/users/{id}","/users").permitAll() // 누구나 접근 허용
                 .antMatchers("/user").hasRole("USER") // USER, ADMIN만 접근 가능
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
