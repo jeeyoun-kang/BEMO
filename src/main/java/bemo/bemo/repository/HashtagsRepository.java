2213e2c8fed7d8d0797ce2e64b9b2ff3e9c29c58
JYKANG <xadamantiumx@naver.com>
2023-02-08 20:59:26 +0900

    0208-2059

--- src/main/java/bemo/bemo/repository/HashtagsRepository.java
+++ src/main/java/bemo/bemo/repository/HashtagsRepository.java
@@ -10,7 +10,10 @@ import java.util.List;
 public interface HashtagsRepository extends JpaRepository<Hashtags, Integer> {
     List<Hashtags> findByMvtitle(String mvtitle);
     List<Hashtags> findByPosts(Posts posts);
+    Long countByContent(String content);
+    @Query("select DISTINCT h.content from Hashtags h")
+    List<String> findContent();
 
-    @Query("select h.mvtitle from Hashtags h where h.content like %:content%")
+    @Query("select DISTINCT h.mvtitle from Hashtags h where h.content like %:content%")
     List<String> findMvtitleByContentContaining(String content);
 }
--- src/main/java/bemo/bemo/repository/PostsRepository.java
+++ src/main/java/bemo/bemo/repository/PostsRepository.java
@@ -1,6 +1,7 @@
 package bemo.bemo.repository;
 
 import bemo.bemo.entity.Posts;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,8 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+
+
     @Query("select p from Posts p where p.mvtitle in :mvtitle")
     List<Posts> findPostsByMvtitle(List<String> mvtitle);
+
     @Query("select p from Posts p order by p.post_id desc")
     List<Posts> findAllDesc(Sort post_id);
+
+
 }
+
