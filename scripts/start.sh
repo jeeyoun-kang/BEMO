271d4e079b6b46ee14d9b15979032a6f5086886c
JYJProgram <33517050+JYJProgram@users.noreply.github.com>
2023-03-02 22:18:34 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,4 +30,6 @@ echo "> $JAR_NAME 실행" >> $DEPLOY_LOG
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다." >> $DEPLOY_LOG
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/$JAR_NAME 2>&1
+
+APP_LOG="$REPOSITORY/application.log"
+nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $APP_LOG 2>&1 &
