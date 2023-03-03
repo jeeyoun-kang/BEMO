97059c7dc0c1a0e0da445e4d311dab73baeb3cd1
Jeeyoun Kang <xadamantiumx@naver.com>
2023-03-03 12:41:00 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -15,13 +15,13 @@ CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
 echo "$TIME_NOW> 컨테이너 ID는 무엇?? ${CONTAINER_ID}" >> $DEPLOY_LOG
 echo "$TIME_NOW> 현재 프로필은 무엇?? ${IDLE_PROFILE}" >> $DEPLOY_LOG
 
-if [ -z ${CONTAINER_ID} ]
-then
-  echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW> docker stop ${IDLE_PROFILE}" >> $DEPLOY_LOG
-  sudo docker stop ${IDLE_PROFILE}
-  echo "$TIME_NOW> docker rm ${IDLE_PROFILE}" >> $DEPLOY_LOG
-  sudo docker rm ${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
-  sleep 5
-fi
+# if [ -z ${CONTAINER_ID} ]
+# then
+#   echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
+# else
+#   echo "$TIME_NOW> docker stop ${IDLE_PROFILE}" >> $DEPLOY_LOG
+#   sudo docker stop ${IDLE_PROFILE}
+#   echo "$TIME_NOW> docker rm ${IDLE_PROFILE}" >> $DEPLOY_LOG
+#   sudo docker rm ${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
+#   sleep 5
+# fi
