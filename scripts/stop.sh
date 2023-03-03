4870c2e2292a73d1fff62c4ffafcc806442ec7c8
Jeeyoun Kang <xadamantiumx@naver.com>
2023-03-03 13:36:48 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,22 +6,16 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 TIME_NOW=$(date +%c)
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+IDLE_PORT=$(find_idle_port)
 
-IDLE_PROFILE=$(find_idle_profile)
+echo "$TIME_NOW> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
+IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
 
-CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
-
-echo "$TIME_NOW> 컨테이너 ID는 무엇?? ${CONTAINER_ID}" >> $DEPLOY_LOG
-echo "$TIME_NOW> 현재 프로필은 무엇?? ${IDLE_PROFILE}" >> $DEPLOY_LOG
-
-# if [ -z ${CONTAINER_ID} ]
-# then
-#   echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
-# else
-#   echo "$TIME_NOW> docker stop ${IDLE_PROFILE}" >> $DEPLOY_LOG
-#   sudo docker stop ${IDLE_PROFILE}
-#   echo "$TIME_NOW> docker rm ${IDLE_PROFILE}" >> $DEPLOY_LOG
-#   sudo docker rm ${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
-#   sleep 5
-# fi
+if [ -z ${IDLE_PID} ]
+then
+  echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW> kill -15 $IDLE_PID" >> $DEPLOY_LOG # Nginx에 연결되어 있지는 않지만 현재 실행 중인 jar 를 Kill 합니다.
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
