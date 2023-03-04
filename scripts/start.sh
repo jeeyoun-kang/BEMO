72db9e0719e629bfd024e17f9b2eb00de1cc59ac
JYKANG <xadamantiumx@naver.com>
2023-03-04 09:01:15 +0900

    0304-0901

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,47 +1,33 @@
 #!/usr/bin/env bash
 
-#REPOSITORY="/home/ec2-user/app"
-#JAR_FILE="$REPOSITORY/spring-webapp.jar"
-#ABSPATH=$(readlink -f $0)
-#ABSDIR=$(dirname $ABSPATH)
-#source ${ABSDIR}/profile.sh
-#TIME_NOW=$(date +%c)
-#
-#APP_LOG="$PROJECT_ROOT/application.log"
-#ERROR_LOG="$PROJECT_ROOT/error.log"
-#DEPLOY_LOG="$REPOSITORY/deploy.log"
-#
-#IDLE_PORT=$(find_idle_port)
-#
-#echo "$TIME_NOW > Build 파일 복사" >> $DEPLOY_LOG
-#echo "$TIME_NOW > cp $REPOSITORY/*.jar $REPOSITORY/"
-#
-#cp $REPOSITORY/build/libs/*.jar $JAR_FILE      # 새로운 jar file 계속 덮어쓰기
-#
-#echo "$TIME_NOW > $JAR_NAME 에 실행권한 추가" >> $DEPLOY_LOG
-#chmod +x $JAR_NAME
-#
-#echo "$TIME_NOW > JAR Name: $JAR_FILE" >> $DEPLOY_LOG
-#echo "$TIME_NOW > $JAR_FILE 실행" >> $DEPLOY_LOG
-#
-#IDLE_PROFILE=$(find_idle_profile)
-#
-#echo "$TIME_NOW > $JAR_FILE 를 profile=$IDLE_PROFILE 로 실행합니다." >> $DEPLOY_LOG
-#nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-PROJECT_ROOT="/home/ec2-user/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+REPOSITORY="/home/ec2-user/app"
+JAR_FILE="$REPOSITORY/spring-webapp.jar"
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+TIME_NOW=$(date +%c)
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+DEPLOY_LOG="$REPOSITORY/deploy.log"
 
-TIME_NOW=$(date +%c)
+IDLE_PORT=$(find_idle_port)
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW > Build 파일 복사" >> $DEPLOY_LOG
+echo "$TIME_NOW > cp $REPOSITORY/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/build/libs/*.jar $JAR_FILE      # 새로운 jar file 계속 덮어쓰기
+
+echo "$TIME_NOW > $JAR_NAME 에 실행권한 추가" >> $DEPLOY_LOG
+chmod +x $JAR_NAME
+
+echo "$TIME_NOW > JAR Name: $JAR_FILE" >> $DEPLOY_LOG
+echo "$TIME_NOW > $JAR_FILE 실행" >> $DEPLOY_LOG
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "$TIME_NOW > $JAR_FILE 를 profile=$IDLE_PROFILE 로 실행합니다." >> $DEPLOY_LOG
+nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,39 +1,21 @@
 #!/usr/bin/env bash
 
-#PROJECT_ROOT="/home/ec2-user/app"
-#ABSPATH=$(readlink -f $0)
-#ABSDIR=$(dirname $ABSPATH)
-#source ${ABSDIR}/profile.sh
-#TIME_NOW=$(date +%c)
-#
-#IDLE_PORT=$(find_idle_port)
-#
-#echo "$TIME_NOW> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
-#IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
-#
-#if [ -z ${IDLE_PID} ]
-#then
-#  echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
-#else
-#  echo "$TIME_NOW> kill -15 $IDLE_PID" >> $DEPLOY_LOG # Nginx에 연결되어 있지는 않지만 현재 실행 중인 jar 를 Kill 합니다.
-#  kill -15 ${IDLE_PID}
-#  sleep 5
-#fi
-
 PROJECT_ROOT="/home/ec2-user/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
-
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 TIME_NOW=$(date +%c)
 
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+IDLE_PORT=$(find_idle_port)
 
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+echo "$TIME_NOW> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
+IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "$TIME_NOW> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -9 $CURRENT_PID
+  echo "$TIME_NOW> kill -15 $IDLE_PID" >> $DEPLOY_LOG # Nginx에 연결되어 있지는 않지만 현재 실행 중인 jar 를 Kill 합니다.
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi
