9be5cd7ac4a64088cae483c014101f65143e0a2b
JYJProgram <33517050+JYJProgram@users.noreply.github.com>
2023-03-04 18:13:57 +0900

    0304-1813
    전과같음

--- src/main/java/bemo/bemo/controller/MainController.java
+++ src/main/java/bemo/bemo/controller/MainController.java
@@ -51,9 +51,8 @@ public class MainController {
         List<String> profile = Arrays.asList(env.getActiveProfiles());
         List<String> realProfile = Arrays.asList("real1", "real2");
         String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
-        System.out.println("profile "+profile);
-//        model.addAttribute("port",profile.stream().filter(realProfile::contains).findAny().orElse(defaultProfile));
-        model.addAttribute("port",profile);
+        model.addAttribute("port",profile.stream().filter(realProfile::contains).findAny().orElse(defaultProfile));
+        model.addAttribute("allPort",profile);
 
         if (principalDetails != null) {
             model.addAttribute("userinfo", principalDetails.getUsername());
